84caa882480e2774dcaaf5c57d1c2ec706c840b8
Akashneel Raychaudhuri <94317530+Alph3ga@users.noreply.github.com>
2025-04-13 22:00:16 +0530

    Hotfix/UUID representation (#6)
    * fix(backend) Add Uuidrepresentation parameter
    
    * fix(backend) Changed parameter to string

--- backend/src/db/db_init.py
+++ backend/src/db/db_init.py
@@ -9,7 +9,6 @@ def init_db():
         username= config["MONGO_USER"],
         password= config["MONGO_PASSWORD"],
         host= config["MONGO_HOST"],
-        port= int(config["MONGO_PORT"],
-        uuidRepresentation=UuidRepresentation.STANDARD
-        )
+        port= int(config["MONGO_PORT"]),
+        uuidRepresentation= 'standard'
     )
