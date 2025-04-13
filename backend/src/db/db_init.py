22af623bb2ff7dfd89128d0b3c72ac48e5b2c5ae
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-04-13 21:41:46 +0530

    fix(backend) Add Uuidrepresentation parameter

--- backend/src/db/db_init.py
+++ backend/src/db/db_init.py
@@ -1,4 +1,5 @@
 from mongoengine import connect
+from bson.binary import UuidRepresentation
 from dotenv import dotenv_values
 
 def init_db():
@@ -8,5 +9,7 @@ def init_db():
         username= config["MONGO_USER"],
         password= config["MONGO_PASSWORD"],
         host= config["MONGO_HOST"],
-        port= int(config["MONGO_PORT"])
+        port= int(config["MONGO_PORT"],
+        uuidRepresentation=UuidRepresentation.STANDARD
+        )
     )
