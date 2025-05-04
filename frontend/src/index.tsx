7b6fe8a9ff8cc8758d40111cd80b04d6def753b7
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-05-04 18:45:16 +0530

    stash commit to merge yarn.lock

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <App />
