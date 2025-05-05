a3c6f9feaf7acc3ae1bd207f5becc528c3fed94b
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-05-06 00:10:01 +0530

    feat(backend): Add filtered paginated GET endpoint
    Filter on name, brand, category, price, and quantity

--- backend/src/services/product_service.py
+++ backend/src/services/product_service.py
@@ -135,3 +135,45 @@ class ProductService:
         """
         product = Product.objects.get(id=product_id)
         return product.set_price(amount)
+
+    @staticmethod
+    def get_product_filtered(name: str, category: str, brand: str, price_less_than_e: int, \
+        price_greater_than_e: int, quantity_less_than_e: int, quantity_greater_than_e: int):
+        """
+        Retrieves products from the database filtered by name, category, brand, price, and quantity.
+
+        Args:
+            name (str): Name of the product to filter by. Use an empty string to ignore.
+            category (str): Category of the product to filter by. Use an empty string to ignore.
+            brand (str): Brand of the product to filter by. Use an empty string to ignore.
+            price_less_than_e (int): Upper bound for product price (inclusive). Use -1 to ignore.
+            price_greater_than_e (int): Lower bound for product price (inclusive). Use -1 to ignore.
+            quantity_less_than_e (int): Upper bound for product quantity (inclusive). Use -1 to ignore.
+            quantity_greater_than_e (int): Lower bound for product quantity (inclusive). Use -1 to ignore.
+
+        Returns:
+            QuerySet: A queryset of Product objects that match the specified filters.
+        """
+
+        data= Product.objects
+
+        if not name == "":
+            data= data.filter(name= name)
+        if not brand == "":
+            data= data.filter(brand= brand)
+        if not category == "":
+            data= data.filter(category= category)
+        
+        if price_less_than_e > -1:
+            data= data.filter(price__lte= price_less_than_e)
+        if price_greater_than_e > -1:
+            data= data.filter(price__gte= price_greater_than_e)
+        
+        if quantity_less_than_e > -1:
+            data= data.filter(quantity__lte= quantity_less_than_e)
+        if quantity_greater_than_e > -1:
+            data= data.filter(quantity__gte= quantity_greater_than_e)
+        
+
+        return data
+        
